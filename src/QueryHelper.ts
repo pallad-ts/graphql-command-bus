af11cfe0a527acd9a2e6148a3a37378e8230e457
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2022-03-22 09:34:30 +0100

    Updated deps

--- src/QueryHelper.ts
+++ src/QueryHelper.ts
@@ -1,106 +1,106 @@
-import {GraphQLInputObjectType, Thunk, GraphQLNonNull, GraphQLList, GraphQLEnumType, GraphQLInt, GraphQLString, GraphQLObjectType} from 'graphql';
-import {GraphQLInputFieldConfigMap} from 'graphql/type/definition';
+import {GraphQLInputObjectType, ThunkObjMap, GraphQLNonNull, GraphQLList, GraphQLEnumType, GraphQLInt, GraphQLString, GraphQLObjectType} from 'graphql';
+import {GraphQLInputFieldConfig} from 'graphql/type/definition';
 import {ObjectTypeComposer} from 'graphql-compose';
 import {QueryResultMetaPaginationByNode, QueryResultMetaPaginationByOffset} from './GQL';
 
 
 const SORT_DIRECTION = new GraphQLEnumType({
-    name: 'SortDirection',
-    values: {
-        ASC: {
-            value: 'ASC'
-        },
-        DESC: {
-            value: 'DESC'
-        }
-    }
+	name: 'SortDirection',
+	values: {
+		ASC: {
+			value: 'ASC'
+		},
+		DESC: {
+			value: 'DESC'
+		}
+	}
 });
 
 function createSortByDefinitionType(prefix: string, fields: string[]) {
-    return new GraphQLInputObjectType({
-        name: prefix + '_Query_Sort',
-        fields: {
-            direction: {type: SORT_DIRECTION},
-            field: {
-                type: new GraphQLEnumType({
-                    name: prefix + '_Query_Sort_Field',
-                    values: fields.reduce((result, field) => {
-                        result[field] = {value: field};
-                        return result;
-                    }, {} as any)
-                })
-            }
-        }
-    })
+	return new GraphQLInputObjectType({
+		name: prefix + '_Query_Sort',
+		fields: {
+			direction: {type: SORT_DIRECTION},
+			field: {
+				type: new GraphQLEnumType({
+					name: prefix + '_Query_Sort_Field',
+					values: fields.reduce((result, field) => {
+						result[field] = {value: field};
+						return result;
+					}, {} as any)
+				})
+			}
+		}
+	})
 }
 
 export namespace QueryHelper {
-    export function createQueryType(options: QueryOptions) {
+	export function createQueryType(options: QueryOptions) {
 
-        const prefix = options.filters.name
-            .replace(/_Filters/i, '')
-            .replace(/_Query/i, '');
+		const prefix = options.filters.name
+			.replace(/_Filters/i, '')
+			.replace(/_Query/i, '');
 
-        const fields: Thunk<GraphQLInputFieldConfigMap> = {
-            filters: {type: new GraphQLNonNull(options.filters)}
-        };
+		const fields: ThunkObjMap<GraphQLInputFieldConfig> = {
+			filters: {type: new GraphQLNonNull(options.filters)}
+		};
 
-        if (options.sortableFields) {
-            fields.sortBy = {
-                type:
-                    options.singleSortable ?
-                        createSortByDefinitionType(prefix, options.sortableFields) :
-                        new GraphQLList(createSortByDefinitionType(prefix, options.sortableFields))
-            };
-        }
-        if (options.pagination) {
-            fields.limit = {type: GraphQLInt};
-            if ('byOffset' in options.pagination) {
-                fields.offset = {type: GraphQLInt};
-            } else if ('byNode' in options.pagination) {
-                fields.after = {type: GraphQLString};
-                fields.before = {type: GraphQLString}
-            }
-        }
+		if (options.sortableFields) {
+			fields.sortBy = {
+				type:
+					options.singleSortable ?
+						createSortByDefinitionType(prefix, options.sortableFields) :
+						new GraphQLList(createSortByDefinitionType(prefix, options.sortableFields))
+			};
+		}
+		if (options.pagination) {
+			fields.limit = {type: GraphQLInt};
+			if ('byOffset' in options.pagination) {
+				fields.offset = {type: GraphQLInt};
+			} else if ('byNode' in options.pagination) {
+				fields.after = {type: GraphQLString};
+				fields.before = {type: GraphQLString}
+			}
+		}
 
-        return new GraphQLInputObjectType({
-            name: prefix + '_Query',
-            fields
-        });
-    }
+		return new GraphQLInputObjectType({
+			name: prefix + '_Query',
+			fields
+		});
+	}
 
-    export function createQueryResultType(type: ObjectTypeComposer<any>, pagination?: PaginationOptions) {
-        const metaType = getMetaTypeForPaginationOptions(pagination);
-        return new GraphQLObjectType({
-            name: type.getTypeName() + '_Query_Result',
-            fields: {
-                results: {type: type.getTypePlural().getType()},
-                ...(metaType ? {meta: {type: metaType}} : {})
-            }
-        });
-    }
+	export function createQueryResultType(type: ObjectTypeComposer<any>, pagination?: PaginationOptions) {
+		const metaType = getMetaTypeForPaginationOptions(pagination);
+		return new GraphQLObjectType({
+			name: type.getTypeName() + '_Query_Result',
+			fields: {
+				results: {type: type.getTypePlural().getType()},
+				...(metaType ? {meta: {type: metaType}} : {})
+			}
+		});
+	}
 
-    export type PaginationOptions = { byNode: true } | { byOffset: true };
+	export type PaginationOptions = { byNode: true } | { byOffset: true };
 
-    export interface QueryOptions {
-        filters: GraphQLInputObjectType,
-        sortableFields?: string[],
-        singleSortable?: boolean;
-        pagination?: PaginationOptions
-    }
+	export interface QueryOptions {
+		filters: GraphQLInputObjectType,
+		sortableFields?: string[],
+		singleSortable?: boolean;
+		pagination?: PaginationOptions
+	}
 }
 
 function getMetaTypeForPaginationOptions(pagination?: QueryHelper.PaginationOptions) {
-    if (!pagination) {
-        return;
-    }
+	if (!pagination) {
+		return;
+	}
 
-    if ('byNode' in pagination && pagination.byNode) {
-        return QueryResultMetaPaginationByNode;
-    }
+	if ('byNode' in pagination && pagination.byNode) {
+		return QueryResultMetaPaginationByNode;
+	}
 
-    if ('byOffset' in pagination && pagination.byOffset) {
-        return QueryResultMetaPaginationByOffset;
-    }
+	if ('byOffset' in pagination && pagination.byOffset) {
+		return QueryResultMetaPaginationByOffset;
+	}
 }
 
